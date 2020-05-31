e290395503bf3beb9be7383baca2c1ebdfea0810
rob <robcannonxyz@gmail.com>
2020-05-31 16:31:32 -0700

    adding basic cookiecutter operator

---
+++ cookiecutter/operators/cookiecutter.py
@@ -0,0 +1,29 @@
+import cookiecutter
+from cookiecutter.operators import BaseOperator
+
+
+class CookiecutterOperator(BaseOperator):
+    """Operator for PyInquirer type prompts."""
+
+    type = 'cookiecutter'
+
+    def __init__(self, operator_dict, context=None):
+        """Initialize PyInquirer Hook."""  # noqa
+        super(CookiecutterOperator, self).__init__(
+            operator_dict=operator_dict, context=context
+        )
+
+    def execute(self):
+        """Run the prompt."""  # noqa
+
+        if 'checkout' in self.operator_dict:
+            checkout = self.operator_dict['checkout']
+        else:
+            checkout = None
+
+        result = cookiecutter.main.cookiecutter(
+            template=self.operator_dict['template'],
+            checkout=checkout,
+        )
+
+        return result
