1686a3d48dace6282d2447a681395ce412c1ede6
rob <robcannonxyz@gmail.com>
2020-06-09 00:29:50 -0700

    adding input and expand operators

---
+++ cookiecutter/operators/input.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+"""Functions for generating a project from a project template."""
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import logging
+from PyInquirer import prompt
+
+from cookiecutter.operators import BaseOperator
+
+logger = logging.getLogger(__name__)
+
+
+class InquirerInputOperator(BaseOperator):
+    """Operator for PyInquirer type prompts."""
+
+    type = 'input'
+
+    def __init__(self, operator_dict, context=None, no_input=False):
+        """Initialize PyInquirer Hook."""  # noqa
+        super(InquirerInputOperator, self).__init__(
+            operator_dict=operator_dict, context=context, no_input=no_input
+        )
+
+    def execute(self):
+        """Run the prompt."""  # noqa
+        if not self.no_input:
+            if 'name' not in self.operator_dict:
+                self.operator_dict.update({'name': 'tmp'})
+                return prompt([self.operator_dict])['tmp']
+            else:
+                return prompt([self.operator_dict])
+        elif 'default' in self.operator_dict:
+            return self.operator_dict['default']
+        else:
+            # When no_input then return empty list
+            return []
