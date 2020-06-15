a0aff94ff387366f6dd57bc8cab49b6ed0d038b8
rob <robcannonxyz@gmail.com>
2020-06-14 19:07:10 -0700

    removing python2 support in setup and travis and pin requirements

--- setup.py
+++ setup.py
@@ -34,8 +34,8 @@ requirements = [
     'requests>=2.23.0',
     'six>=1.10',
     'MarkupSafe<2.0.0',
-    'PyInquirer',
-    'PyYAML',
+    'PyInquirer>=1.0.3',
+    'PyYAML>=5.3',
 ]
 
 if sys.argv[-1] == 'readme':
@@ -60,7 +60,7 @@ setup(
     package_dir={'cookiecutter': 'cookiecutter'},
     entry_points={'console_scripts': ['nukikata = cookiecutter.__main__:main']},
     include_package_data=True,
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires='>=3.6',
     install_requires=requirements,
     extras_require={':python_version<"3.3"': ['whichcraft>=0.4.0']},
     license='BSD',
@@ -72,10 +72,7 @@ setup(
         "Natural Language :: English",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
