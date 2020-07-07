64f01f052ff9e6db0320fad1ccab67ddb2cd4241
rob <robcannonxyz@gmail.com>
2020-07-06 22:57:38 -0700

    adding index and fixes to pyinquirer list operator

--- cookiecutter/operators/list.py
+++ cookiecutter/operators/list.py
@@ -27,7 +27,47 @@ class InquirerListOperator(BaseOperator):
         )
 
     def execute(self):
-        """Run the prompt."""  # noqa
+        """Run the pyinquirer list style prompt.
+
+        Exposes options to output value or index of choice to be used elsewhere.
+        """
+        if 'index' not in self.operator_dict:
+            self.operator_dict['index'] = False
+
+        # Figure out what type of dictionary it is
+        choices_type = None
+        for i, v in enumerate(self.operator_dict['choices']):
+            if i != 0 and type(v) != choices_type:
+                raise ValueError("All items need to be of the same type")
+            choices_type = type(v)
+
+        # if isinstance(self.operator_dict['choices'], list):
+        if choices_type == str:
+            answer = self._run_prompt()
+            if self.operator_dict['index']:
+                return self.operator_dict['choices'].index(answer)
+            else:
+                return answer
+        elif choices_type == dict:
+            choices = []
+            for i in self.operator_dict['choices']:
+                choices.append(i[list(i.keys())[0]])
+
+            choices_dict = self.operator_dict['choices']
+            self.operator_dict['choices'] = choices
+
+            answer = self._run_prompt()
+            for i in choices_dict:
+                val = list(i.keys())[0]
+                if i[val] == answer:
+                    if self.operator_dict['index']:
+                        return self.operator_dict['choices'].index(answer)
+                    else:
+                        return val
+        else:
+            raise ValueError("Choices must be dict with ")
+
+    def _run_prompt(self):
         if 'name' not in self.operator_dict:
             self.operator_dict.update({'name': 'tmp'})
             return prompt([self.operator_dict])['tmp']
