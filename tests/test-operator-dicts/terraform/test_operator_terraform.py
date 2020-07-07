e4a0316f5a00bdcf33fb324378ca9e95128168c0
rob <robcannonxyz@gmail.com>
2020-07-06 22:00:32 -0700

    adding terraform operator init

---
+++ tests/test-operator-dicts/terraform/test_operator_terraform.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""Tests dict input objects for `cookiecutter.operator.aws.ec2_meta` module."""
+
+import os
+from cookiecutter.main import cookiecutter
+
+
+def test_operator_terraform(monkeypatch, tmpdir):
+    """Verify Jinja2 time extension work correctly."""
+    monkeypatch.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+    context = cookiecutter('.', no_input=True, output_dir=str(tmpdir))
+
+    assert context
