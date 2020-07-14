1ccb954abee7d42979f90c96d869114f7df783bb
rob <robcannonxyz@gmail.com>
2020-07-14 10:31:33 -0700

    adding table operator and test

---
+++ cookiecutter/operators/table.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+"""Operator plugin that inherits a base class and is made available through `type`."""
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import logging
+from rich.console import Console
+from rich.table import Table
+
+from cookiecutter.operators import BaseOperator
+
+logger = logging.getLogger(__name__)
+
+
+class TableOperator(BaseOperator):
+    """
+    Operator for creating tables with rich - github.com/willmcgugan/rich.
+
+    :param column_names: List of column names
+    :param contents: List of lists to put into columns / rows
+    :param contents_split: List of strings to separate into columns based on `separator`
+    :param separator: A string to separate the strings in the contents
+    :param sort: Boolean to sort contents or contents_split
+    """
+
+    type = 'table'
+
+    def __init__(self, operator_dict, context=None, context_key=None, no_input=False):
+        """Initialize operator."""
+        super(TableOperator, self).__init__(
+            operator_dict=operator_dict,
+            context=context,
+            no_input=no_input,
+            context_key=context_key,
+        )
+
+    def execute(self):
+        """Run the operator."""
+        if self.operator_dict['sort'] if 'sort' in self.operator_dict else False:
+            if 'contents' in self.operator_dict:
+                self.operator_dict['contents'].sort()
+            if 'contents_split' in self.operator_dict:
+                self.operator_dict['contents_split'].sort()
+
+        console = Console()
+
+        self.operator_dict['column_names'] = (
+            self.operator_dict['column_names']
+            if 'column_names' in self.operator_dict
+            else []
+        )
+        if len(self.operator_dict['column_names']) > 0:
+            table = Table(show_header=True, header_style="bold magenta")
+            for c in self.operator_dict['column_names']:
+                table.add_column(c)
+        else:
+            table = Table()
+
+        if 'contents_split' in self.operator_dict:
+            for i in self.operator_dict['contents_split']:
+                table.add_row(
+                    *set(
+                        i.split(
+                            self.operator_dict['separator'][
+                                0 : len(self.operator_dict['column_names'])  # noqa
+                            ]
+                        )
+                    )
+                )
+
+        if 'contents' in self.operator_dict:
+            for i in self.operator_dict['contents']:
+                table.add_row(*set(i))
+
+        console.print(table)
---
+++ tests/test-operator-dicts/print/test_operator_table.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Tests dict input objects for `cookiecutter.operator.aws.ec2_meta` module."""
+
+import os
+from cookiecutter.main import cookiecutter
+
+
+def test_operator_terraform(monkeypatch, tmpdir):
+    """Verify Jinja2 time extension work correctly."""
+    monkeypatch.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+    context = cookiecutter(
+        '.', context_file='table_split.yaml', no_input=True, output_dir=str(tmpdir)
+    )
+
+    assert context
