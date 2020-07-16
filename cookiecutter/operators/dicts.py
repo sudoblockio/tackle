4051b1a9a61d6c250f3c88cf0138e3322da5f6f3
rob <robcannonxyz@gmail.com>
2020-07-15 17:06:26 -0700

    adding dict operator

---
+++ cookiecutter/operators/dicts.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+"""Operator plugin that inherits a base class and is made available through `type`."""
+from __future__ import unicode_literals
+from __future__ import print_function
+
+import logging
+
+from cookiecutter.operators import BaseOperator
+
+logger = logging.getLogger(__name__)
+
+
+class DictUpdateOperator(BaseOperator):
+    """
+    Operator for updating dict objects with items.
+
+    :param dict: The input dict to update
+    :param input: A dict to update the input `dict`
+    :param inputs: A list of dicts to update `dict` with
+    :return An updated dict object.
+    """
+
+    type = 'update'
+
+    def __init__(self, operator_dict, context=None, context_key=None, no_input=False):
+        """Initialize operator."""
+        super(DictUpdateOperator, self).__init__(
+            operator_dict=operator_dict,
+            context=context,
+            no_input=no_input,
+            context_key=context_key,
+        )
+
+    def execute(self):
+        """Print the statement."""
+        if 'inputs' in self.operator_dict:
+            for i in self.operator_dict['inputs']:
+                self.operator_dict['dict'].update(i)
+        if 'input' in self.operator_dict:
+            self.operator_dict['dict'].update(self.operator_dict['input'])
+
+        return self.operator_dict['dict']
