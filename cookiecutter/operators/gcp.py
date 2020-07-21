3615fbea74cb1e2c50feb50853efadf0a72310a3
Richard Mah <dick.mah@gmail.com>
2020-07-20 21:22:21 -0400

    Add GCP AZ operator

--- cookiecutter/operators/gcp.py
+++ cookiecutter/operators/gcp.py
@@ -34,3 +34,53 @@ class GcpRegionsOperator(BaseOperator):
         ]
 
         return regions
+
+
+class GcpAzsOperator(BaseOperator):
+    """
+    Operator for retrieving the availability zones in a given region.
+
+    :param region: A region to search in
+    :param regions: A list of regions to search in
+    :return: A list of availability zones
+    """
+
+    type = 'gcp_azs'
+
+    def __init__(self, *args, **kwargs):  # noqa
+        super(GcpAzsOperator, self).__init__(*args, **kwargs)
+
+    def _execute(self):
+        client = build('compute', 'v1')
+
+        if 'region' in self.operator_dict:
+            azs = self._call_azs(
+                client, self.operator_dict['region'], self.operator_dict['gcp_project']
+            )
+            azs.sort()
+            return azs
+
+        elif 'regions' in self.operator_dict:
+            output = {}
+            for r in self.operator_dict['regions']:
+                azs = self._call_azs(client, r, self.operator_dict['gcp_project'])
+                azs.sort()
+                output.update({r: azs})
+            return output
+
+    @staticmethod
+    def _call_azs(client, region, project):
+        region_uri_stub = (
+            "\"https://www.googleapis.com/compute/v1/projects/" + project + "/regions/"
+        )
+        availability_zones = [
+            item['name']
+            for item in client.zones()
+            .list(
+                project=project,
+                filter="region=" + region_uri_stub + region + "\" AND status=\"UP\"",
+            )
+            .execute()['items']
+        ]
+        availability_zones.sort()
+        return availability_zones
