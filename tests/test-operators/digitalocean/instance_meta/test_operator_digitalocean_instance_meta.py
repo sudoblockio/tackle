df8dfc0c869c80cc198cc57bab237e9190c1ba50
Richard Mah <dick.mah@gmail.com>
2020-07-23 20:06:46 -0400

    Add DO operator tests and add env to tox config

---
+++ tests/test-operators/digitalocean/instance_meta/test_operator_digitalocean_instance_meta.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+"""Tests dict input objects for `cookiecutter.operator.gcp.instance_meta` module."""
+
+import os
+from cookiecutter.main import cookiecutter
+
+
+def test_operator_gcp_instance_meta(monkeypatch, tmpdir):
+    """Verify Jinja2 time extension work correctly."""
+    monkeypatch.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+    context = cookiecutter('.', no_input=True, output_dir=str(tmpdir))
+
+    assert len(context['instance_types']) > 1
+
+
+def test_operator_gcp_instance_meta_instance_family(monkeypatch, tmpdir):
+    """Verify Jinja2 time extension work correctly."""
+    monkeypatch.chdir(os.path.abspath(os.path.dirname(__file__)))
+
+    context = cookiecutter(
+        '.', context_file='instance_family.yaml', no_input=True, output_dir=str(tmpdir),
+    )
+
+    assert len(context['instance_types']) > 1
+    assert len(context['instance_types']) < 20
+    assert "gd-2vcpu-8gb" not in context['instance_types']
