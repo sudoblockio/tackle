1ea814163d96af9072ec75ebc54895bd6323f9e5
rob <robcannonxyz@gmail.com>
2020-08-03 18:18:20 -0700

    adding move operator

--- cookiecutter/operators/file.py
+++ cookiecutter/operators/file.py
@@ -15,6 +15,16 @@ from cookiecutter.operators import BaseOperator
 logger = logging.getLogger(__name__)
 
 
+def create_directory_tree(src, dst):
+    """Create the file path if it does not exist."""
+    if isinstance(src, list) and not os.path.isdir(dst):
+        Path(dst).mkdir(parents=True, exist_ok=True)
+    elif isinstance(src, str) and not os.path.isdir(os.path.dirname(dst)):
+        Path(os.path.basename(dst)).mkdir(parents=True, exist_ok=True)
+    elif isinstance(src, dict):
+        raise NotImplementedError
+
+
 class CopyOperator(BaseOperator):
     """
     Operator for updating dict objects with items.
@@ -31,9 +41,12 @@ class CopyOperator(BaseOperator):
     def __init__(self, *args, **kwargs):  # noqa
         super(CopyOperator, self).__init__(*args, **kwargs)
         if isinstance(self.operator_dict['src'], str):
-            self.src = os.path.abspath(self.operator_dict['src'])
+            self.src = os.path.abspath(os.path.expanduser(self.operator_dict['src']))
         if isinstance(self.operator_dict['src'], list):
-            self.src = [os.path.abspath(f) for f in self.operator_dict['src']]
+            self.src = [
+                os.path.abspath(os.path.expanduser(f))
+                for f in self.operator_dict['src']
+            ]
 
         self.dst = os.path.abspath(self.operator_dict['dst'])
         self.create_path = (
@@ -44,14 +57,7 @@ class CopyOperator(BaseOperator):
 
     def _execute(self):
         if self.create_path:
-            if isinstance(self.src, list) and not os.path.isdir(self.dst):
-                Path(self.dst).mkdir(parents=True, exist_ok=True)
-            elif isinstance(self.src, str) and not os.path.isdir(
-                os.path.dirname(self.dst)
-            ):
-                Path(os.path.basename(self.dst)).mkdir(parents=True, exist_ok=True)
-            elif isinstance(self.src, dict):
-                raise NotImplementedError
+            create_directory_tree(self.src, self.dst)
 
         if isinstance(self.src, str):
             self._copy_file_or_dir(self.src, self.dst)
@@ -67,3 +73,46 @@ class CopyOperator(BaseOperator):
             copy_tree(source, dest)
         elif os.path.isfile(source):
             shutil.copyfile(source, dest)
+
+
+class MoveOperator(BaseOperator):
+    """
+    Operator for updating dict objects with items.
+
+    :param src: String or list of sources, either a directories or files
+    :param dst: String for path to copy to
+    :param create_path: Boolean to create the directory path if it does not exist.
+        Defaults to true
+    :return: None
+    """
+
+    type = 'move'
+
+    def __init__(self, *args, **kwargs):  # noqa
+        super(MoveOperator, self).__init__(*args, **kwargs)
+        if isinstance(self.operator_dict['src'], str):
+            self.src = os.path.abspath(os.path.expanduser(self.operator_dict['src']))
+        if isinstance(self.operator_dict['src'], list):
+            self.src = [
+                os.path.abspath(os.path.expanduser(f))
+                for f in self.operator_dict['src']
+            ]
+
+        self.dst = os.path.abspath(self.operator_dict['dst'])
+        self.create_path = (
+            self.operator_dict['create_path']
+            if 'create_path' in self.operator_dict
+            else True
+        )
+
+    def _execute(self):
+        if self.create_path:
+            create_directory_tree(self.src, self.dst)
+
+        if isinstance(self.src, str):
+            shutil.move(self.src, self.dst)
+        elif isinstance(self.src, list):
+            for i in self.src:
+                shutil.move(i, os.path.join(self.dst, os.path.basename(i)))
+
+        return None
--- tests/test-operators/file/test_operator_file_copy.py
+++ tests/test-operators/file/test_operator_file_copy.py
@@ -6,8 +6,9 @@ import shutil
 from cookiecutter.main import cookiecutter
 
 
-def _clean_files():
-    files = ['thing.yaml', 'stuff']
+def clean_files():
+    """Clean the run."""
+    files = ['thing.yaml', 'thing2.yaml', 'stuff']
     for f in files:
         if os.path.isfile(f):
             os.remove(f)
@@ -15,14 +16,17 @@ def _clean_files():
             shutil.rmtree(f)
 
 
-def test_operator_file_copy(monkeypatch, tmpdir):
+def test_operator_file(monkeypatch, tmpdir):
     """Verify the operator call works successfully."""
     monkeypatch.chdir(os.path.abspath(os.path.dirname(__file__)))
-    _clean_files()
+    clean_files()
 
     cookiecutter('.', no_input=True, output_dir=str(tmpdir))
 
     assert 'thing.yaml' in os.listdir()
     assert 'stuff' in os.listdir()
 
-    _clean_files()
+    # If the file has been moved properly there should be only one file
+    assert len(os.listdir('stuff')) == 3
+
+    clean_files()
