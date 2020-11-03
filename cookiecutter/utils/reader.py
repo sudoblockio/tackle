67ec11b41466405ca13f587955c8864578711aea
rob <robcannonxyz@gmail.com>
2020-11-03 02:37:06 -0800

    adding try to reader

--- cookiecutter/utils/reader.py
+++ cookiecutter/utils/reader.py
@@ -3,6 +3,8 @@ import json
 import os
 from collections import OrderedDict
 
+from cookiecutter.exceptions import ContextDecodingException
+
 import hcl
 import yaml
 import logging
@@ -23,20 +25,30 @@ def read_config_file(file, file_extension=None):
             file, file_extension
         )
     )
-    if file_extension == 'json':
-        with open(file) as f:
-            config = json.load(f, object_pairs_hook=OrderedDict)
-        return config
-    elif file_extension in ('yaml', 'yml', 'nukirc'):
-        with open(file, encoding='utf-8') as f:
-            config = yaml.safe_load(f)
-        return config
-    elif file_extension == 'hcl':
-        with open(file) as f:
-            config = hcl.loads(f.read())
-        return config
-    else:
-        raise ValueError(
-            'Unable to parse file {}. Error: Unsupported extension (json/yaml only)'
-            ''.format(file)
-        )  # noqa
+    try:
+        if file_extension == 'json':
+            with open(file) as f:
+                config = json.load(f, object_pairs_hook=OrderedDict)
+            return config
+        elif file_extension in ('yaml', 'yml', 'nukirc'):
+            with open(file, encoding='utf-8') as f:
+                config = yaml.safe_load(f)
+            return config
+        elif file_extension == 'hcl':
+            with open(file) as f:
+                config = hcl.loads(f.read())
+            return config
+        else:
+            raise ValueError(
+                'Unable to parse file {}. Error: Unsupported extension (json/yaml only)'
+                ''.format(file)
+            )  # noqa
+
+    except ValueError as e:
+        # JSON decoding error.  Let's throw a new exception that is more
+        # friendly for the developer or user.
+        our_exc_message = (
+            'JSON decoding error while loading "{0}".  Decoding'
+            ' error details: "{1}"'.format(file, str(e))
+        )
+        raise ContextDecodingException(our_exc_message)
