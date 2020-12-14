f308ff99a9153589fc15743d677999e2c4be089a
rob <robcannonxyz@gmail.com>
2020-12-13 18:16:39 -0800

    adding hook use logic and tests

--- cookiecutter/parser/hooks.py
+++ cookiecutter/parser/hooks.py
@@ -3,140 +3,155 @@
 """Functions for discovering and executing various cookiecutter operators."""
 from __future__ import print_function
 import logging
-import inspect
+from PyInquirer import prompt
+from pydantic.error_wrappers import ValidationError
 
-from cookiecutter.exceptions import InvalidOperatorType
 from cookiecutter.render import render_variable
-
-from cookiecutter.operators import *  # noqa
-from cookiecutter.operators import BaseOperator
+from cookiecutter.parser.providers import get_hook
+from cookiecutter.utils.conditionals import evaluate_when
+from cookiecutter.exceptions import HookCallException
+from cookiecutter.models import BaseHook
 
 from typing import TYPE_CHECKING
-
 if TYPE_CHECKING:
     from cookiecutter.models import Mode, Context, Source
 
-
 logger = logging.getLogger(__name__)
-post_gen_operator_list = []
+
+KEYWORDS = {'when', 'loop', 'chdir' 'existing_context', ''}
 
 
-def run_operator(c: 'Context', m: 'Mode', s: 'Source'):
+def run_operator(context: 'Context', mode: 'Mode'):
     """Run operator."""
-    if c.input_dict is None:
-        c.input_dict = {}
-    operator_output = None
-    delayed_output = None
+    if context.input_dict is None:
+        context.input_dict = {}
 
-    # hook_list = s.hook_list
+    Hook = get_hook(context.hook_dict['type'], context)
 
-    operator_list = BaseOperator.__subclasses__()
+    try:
+        # Take the items out of the global context and mode if they are
+        # declared in the hook dict.
+        exclude_context = {
+            i for i in context.dict().keys() if i in context.hook_dict.keys()
+        }
+        exclude_context.update({'env'})
+        exclude_mode = {i for i in mode.dict().keys() if i in context.hook_dict.keys()}
+        exclude_mode = exclude_mode if exclude_mode != set() else {'tmp'}
 
-    for o in operator_list:
-        if (
-            c.hook_dict['type'] == inspect.signature(o).parameters['type'].default
-        ):  # noqa
-            logger.debug("Using the %s operator" % c.hook_dict['type'])
+        if 'no_input' in context.hook_dict:
+            print()
 
-            overrides = ['context', 'context_key', 'no_input', 'cc_dict']
-            for override in overrides:
-                if override in c.hook_dict:
-                    exec(override + " = hook_dict[override]")
-                    c.hook_dict.pop(override)
-
-            operator = o(
-                **c.hook_dict,
-                context=c.input_dict,
-                context_key=c.context_key,
-                no_input=m.no_input,
-                cc_dict=c.output_dict,
-                key=c.key,
+        hook = Hook(
+            **context.hook_dict,
+            **context.dict(exclude=exclude_context),
+            **mode.dict(exclude=exclude_mode),
+        )
+    except ValidationError as e:
+        if 'extra fields not permitted' in e.__repr__():
+            # This is very common and so lets enrich the error to give the user
+            # more context. Return all the fields in the hook by removing all the
+            # base fields.
+            fields = '--> ' + ', '.join(
+                [
+                    i
+                    for i in Hook().dict().keys()
+                    if i not in BaseHook().dict().keys() and i != 'type'
+                ]
             )
-
-            if operator.post_gen_operator:
-                delayed_output = operator
-            else:
-                operator_output = operator.call()
-            break
-        if o == operator_list[-1]:
-            msg = (
-                "The operator %s is not available out of a list of "
-                "the following operators." % c.hook_dict['type'],
-                operator_list,
+            error_out = (
+                f"Error: The field \"{e.raw_errors[0]._loc}\" is not permitted in "
+                f"key=\"{context.key}\".\n"
+                f"Only values accepted are {fields}, plus base fields --> "
+                f"type, when, loop, chdir."
             )
-            raise InvalidOperatorType(msg)
+            raise HookCallException(error_out)
+        else:
+            raise e
 
-    return operator_output, delayed_output
+    if hook.post_gen_operator:
+        return None, hook
+    else:
+        return hook.call(), None
 
 
 def parse_hook(
-    c: 'Context', m: 'Mode', s: 'Source', append_key: bool = False,
+    context: 'Context', mode: 'Mode', s: 'Source', append_key: bool = False,
 ):
     """Parse input dict for loop and when logic and calls hooks.
 
     :return: cc_dict
     """
-    # global post_gen_operator_list
-    if not c.context_key:
-        c.context_key = next(iter(c.input_dict))  # TODO -> wtf
+    if not context.context_key:
+        context.context_key = next(iter(context.input_dict))  # TODO -> wtf
 
-    # env = StrictEnvironment(context=context)
-    logger.debug("Parsing context_key: %s and key: %s" % (c.context_key, c.key))
-    c.hook_dict = c.input_dict[c.context_key][c.key]
+    logger.debug("Parsing context_key: %s and key: %s" %
+                 (context.context_key, context.key))
+    context.hook_dict = context.input_dict[context.context_key][context.key]
 
-    when_condition = evaluate_when(c)
+    when_condition = evaluate_when(context.hook_dict, context)
 
     if when_condition:
         # Extract loop
-        if 'loop' in c.hook_dict:
-            loop_targets = render_variable(c, c.hook_dict['loop'])
-            c.hook_dict.pop('loop')
+        if 'loop' in context.hook_dict:
+            loop_targets = render_variable(context, context.hook_dict['loop'])
+            context.hook_dict.pop('loop')
 
             loop_output = []
             for i, l in enumerate(loop_targets):
-                c.output_dict.update({'index': i, 'item': l})
-                loop_output += [parse_hook(c, m, s, append_key=True)]
+                context.output_dict.update({'index': i, 'item': l})
+                loop_output += [parse_hook(context, mode, s, append_key=True)]
 
-            c.output_dict.pop('item')
-            c.output_dict.pop('index')
-            c.output_dict[c.key] = loop_output
-            return c.output_dict
+            context.output_dict.pop('item')
+            context.output_dict.pop('index')
+            context.output_dict[context.key] = loop_output
+            return context.output_dict
 
-        if 'block' not in c.hook_dict['type']:
-            c.hook_dict = render_variable(c, c.hook_dict)
+        if 'block' not in context.hook_dict['type']:
+            context.hook_dict = render_variable(context, context.hook_dict)
 
         # Run the operator
-        if c.hook_dict['merge'] if 'merge' in c.hook_dict else False:
-            to_merge, post_gen_hook = run_operator(c, m, s)
-            c.output_dict.update(to_merge)
+        if context.hook_dict['merge'] if 'merge' in context.hook_dict else False:
+            to_merge, post_gen_hook = run_operator(context, mode)
+            context.output_dict.update(to_merge)
         else:
-            c.output_dict[c.key], post_gen_hook = run_operator(c, m, s)
+            context.output_dict[context.key], post_gen_hook = run_operator(
+                context, mode
+            )
         if post_gen_hook:
-            c.post_gen_hooks.append(post_gen_hook)
+            context.post_gen_hooks.append(post_gen_hook)
 
         if append_key:
-            return c.output_dict[c.key]
+            return context.output_dict[context.key]
 
-    return c.output_dict
+    return context.output_dict
 
 
-def evaluate_when(c: 'Context'):
-    """Evaluate the when condition and return bool."""
-    if 'when' not in c.hook_dict:
-        return True
-
-    when_raw = c.hook_dict['when']
-    when_condition = False
-    if isinstance(when_raw, str):
-        when_condition = render_variable(c, when_raw)
-    elif isinstance(when_raw, list):
-        # Evaluate lists as successively evalutated 'and' conditions
-        for i in when_raw:
-            when_condition = render_variable(c, i)
-            # If anything is false, then break immediately
-            if not when_condition:
-                break
-
-    c.hook_dict.pop('when')
-
-    return when_condition
+def _evaluate_confirm(context: 'Context'):
+    if 'confirm' in context.hook_dict:
+        if isinstance(context.hook_dict['confirm'], str):
+            return prompt(
+                [
+                    {
+                        'type': 'confirm',
+                        'name': 'tmp',
+                        'message': context.hook_dict['confirm'],
+                    }
+                ]
+            )['tmp']
+        elif isinstance(context.hook_dict['confirm'], dict):
+            when_condition = True
+            if 'when' in context.hook_dict['confirm']:
+                when_condition = evaluate_when(
+                    self, self.env, self.cc_dict, self.context_key,  # noqa
+                )
+            if when_condition:
+                return prompt(
+                    [
+                        {
+                            'type': 'confirm',
+                            'name': 'tmp',
+                            'message': context.hook_dict['confirm']['message'],
+                            'default': context.hook_dict['confirm']['default'],
+                        }
+                    ]
+                )['tmp']
---
+++ tests/parser/hooks/test_parser_hooks.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+"""Tests dict input objects for `cookiecutter.operator.lists` module."""
+import os
+import pytest
+from cookiecutter.main import cookiecutter
+
+
+def test_parser_conditionals_when(change_dir_fixture):
+    """Verify that the `when` keyword acts right."""
+    o = cookiecutter('.', no_input=True, context_file='when.yaml')
+    assert 'blah' in o
+    assert 'foo' not in o
+    assert 'bar' in o
+    assert 'bings' in o
+
+
+def test_loops(change_dir_fixture, tmpdir):
+    """Verify Jinja2 time extension work correctly."""
+    outputs = [x for x in os.listdir('loops') if x.startswith('output')]
+    for o in outputs:
+        os.remove(os.path.join('loops', o))
+    output = cookiecutter('.', no_input=True, context_file='loops.yaml')
+    # output_dir=str(tmpdir),
+    assert len(output['a_list_of_strings']) == 3
+
+    outputs = [x for x in os.listdir('loops') if x.startswith('output')]
+    for o in outputs:
+        os.remove(os.path.join('loops', o))
+
+
+def test_parser_hooks_raises_error_on_bad_hook_input(change_dir_fixture):
+    """Verify that the hook parser raises the right error when a value in the hook
+    dict is not in the hook type."""
+    from cookiecutter.exceptions import HookCallException
+
+    with pytest.raises(HookCallException):
+        cookiecutter('.', context_file='unknown-hook-input.yaml')
+
+
+def test_parser_hooks_raises_error_on_bad_hook_input_type(change_dir_fixture):
+    """
+    Verify that the hook parser raises the right error when a value in the hook
+    dict is not in the hook type.
+    """
+    from pydantic.error_wrappers import ValidationError
+
+    with pytest.raises(ValidationError):
+        cookiecutter('.', context_file='bad-hook-input.yaml')
