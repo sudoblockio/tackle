ed6d5f4220fd43b7dd9f0c94fad98d8c96553dc1
rob <robcannonxyz@gmail.com>
2020-12-13 18:10:54 -0800

    refactoring operator tests

--- tests/providers/azure/test_provider_azure_hooks.py
+++ tests/providers/azure/test_provider_azure_hooks.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """Tests dict input objects for `cookiecutter.operator.aws.regions` module."""
-import os
 from cookiecutter.main import cookiecutter
 
 
-def test_operator_azure_regions(monkeypatch, tmpdir):
+def test_operator_azure_regions(change_dir):
     """Verify the operator call works successfully."""
-    monkeypatch.chdir(os.path.abspath(os.path.dirname(__file__)))
-
-    context = cookiecutter('.', no_input=True, output_dir=str(tmpdir))
-
+    context = cookiecutter('.')
     assert len(context['azs']) > 1
