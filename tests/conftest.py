f31927dbfe83494a1acdc6ff8422fdf0430ac7c7
rob <robcannonxyz@gmail.com>
2020-12-28 23:42:25 -0800

    adding global fixtrues for provider tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -55,6 +55,8 @@ def restore_backup_dir(original_dir, backup_dir, original_dir_found):
 def clean_system(request):
     """Fixture. Simulates a clean system with no configured or cloned cookiecutters.
 
+    TODO: When new config structure changes update this
+
     It runs code which can be regarded as setup code as known from a unittest
     TestCase. Additionally it defines a local function referring to values
     which have been stored to local variables in the setup such as the location
@@ -151,11 +153,13 @@ def user_config_data(user_dir):
     :returns: Dict with name of both user config dirs
     """
     cookiecutters_dir = user_dir.mkdir('cookiecutters')
+    tackle_dir = user_dir.mkdir('.tackle')
     replay_dir = user_dir.mkdir('cookiecutter_replay')
 
     return {
         'cookiecutters_dir': str(cookiecutters_dir),
         'replay_dir': str(replay_dir),
+        'tackle_dir': str(tackle_dir),
     }
 
 
@@ -204,18 +208,6 @@ def clean_output(request):
 
 
 @pytest.fixture(scope='function')
-def change_dir(monkeypatch):
-    """Change to the current directory."""
-    monkeypatch.chdir(os.path.abspath(os.path.curdir))
-
-
-@pytest.fixture(scope='function')
-def change_dir_fixture(monkeypatch):
-    """Change to the current directory."""
-    monkeypatch.chdir(os.path.join(os.path.abspath(os.path.curdir), 'fixtures'))
-
-
-@pytest.fixture(scope='function')
 def load_yaml(request):
     """Return dict of yaml input(s) either str or tuple."""
     if isinstance(request.param, str):
@@ -228,26 +220,3 @@ def load_yaml(request):
             with open(i) as f:
                 output.append(yaml.load(f))
         return output
-
-
-@pytest.fixture(scope='function')
-def inputs(request):
-    if isinstance(request.param, tuple):
-        for i in request.param:
-            yield i
-
-
-from click.testing import CliRunner
-from tackle.__main__ import main
-
-
-@pytest.fixture(scope='session')
-def cli_runner():
-    """Fixture that returns a helper function to run the cookiecutter cli."""
-    runner = CliRunner()
-
-    def cli_main(*cli_args, **cli_kwargs):
-        """Run cookiecutter cli main with the given args."""
-        return runner.invoke(main, cli_args, **cli_kwargs)
-
-    return cli_main
