41c232a806520513fb950502925043ce91a39278
rob <robcannonxyz@gmail.com>
2021-01-15 00:32:52 -0700

    adding first git hooks

--- tackle/providers/git/hooks/meta.py
+++ tackle/providers/git/hooks/meta.py
@@ -4,63 +4,148 @@
 from __future__ import unicode_literals
 from __future__ import print_function
 
+import pathlib
 import os
+import re
+from rich import print
 import logging
 import subprocess
+from collections import MutableMapping
+from PyInquirer import prompt
 
+from pydantic import BaseModel, ValidationError
 from tackle.utils.context_manager import work_in
 from tackle.models import BaseHook
-from typing import Dict, List
+from typing import Dict, Optional
 
 logger = logging.getLogger(__name__)
+
+
+class Repo(BaseModel):
+    src: str
+    branch: Optional[str] = None
+    tag: Optional[str] = None
+
+
+# https://stackoverflow.com/a/6027615/12642712
+def flatten_repo_tree(d, parent_key=''):
+    items = []
+    for k, v in d.items():
+        new_key = os.path.join(parent_key, k)
+
+        try:
+            repo = Repo(**v)
+        except (ValidationError, TypeError):
+            repo = None
+
+        if repo:
+            items.append((new_key, v))
+        elif isinstance(v, MutableMapping):
+            # items.extend(flatten(v, new_key, sep=sep).items())
+            items.extend(flatten_repo_tree(v, new_key).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
 
 
 class MetaGitHook(BaseHook):
     """
     Hook to create meta repo.
-
     """
 
     type: str = 'meta_repo'
+    command: str = None
 
     repos: Dict = None
     repo_tree: Dict = None
 
+    base_url: str = "https://github.com"
+    git_org: str = None
+
     base_dir: str = os.path.abspath(os.path.curdir)
     meta_file: str = '.meta'
-    action: str = None
 
+    def run_git_command(self, repo, folder_name, branch=None):
 
-    def _action(self):
-        if self.action:
-            return
+        if not branch:
+            cmd = f"git {self.command} {repo} {folder_name}"
+        else:
+            cmd = f"git {self.command} {repo} {folder_name} -b {branch}"
 
-    def _git_command(self):
+        print()
         p = subprocess.Popen(
-            self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
         )
         output, err = p.communicate()
+        if err:
+            print(f"Error {err} when running the command {self.command}. Skipping.")
 
-    def _parse_repo_tree(self):
-        def flatten(d, parent_key='', sep='_'):
-            items = []
-            for k, v in d.items():
-                new_key = os.path.join(parent_key, k)
-                if isinstance(v, collections.MutableMapping):
-                    items.extend(flatten(v, new_key, sep=sep).items())
-                else:
-                    items.append((new_key, v))
-            return dict(items)
+    def update_repo_name(self, v):
+        if re.compile(r"(https:\/\/[\w\.]+)|(git@[\w\.]+)").match(v):
+            return v
 
-
-
+        git_parts = v.split('/')
+        if len(git_parts) == 1:
+            if not self.git_org:
+                print(
+                    f"No git org declared and only one item declared in {v}, "
+                    f"must be in form org/repo for abbreviated declarations. Skipping."
+                )
+                return
+            else:
+                return f"{self.base_url}/{self.git_org}/{git_parts[0]}"
+        if len(git_parts) == 2:
+            return f"{self.base_url}/{self.git_org}/{git_parts[0]}"
+        else:
+            print(f"Malformed repo name '{v}' in '{self.key}' key. Skipping.")
 
     def execute(self):
-        with work_in(self.base_dir):
-            for i in self.repo_tree.keys():
-                pass
+        if not self.command:
+            if not self.no_input:
+                git_commands = [
+                    '<custom input>',
+                    'clone',
+                    'pull',
+                    'branch',
+                    'rebase',
+                    'checkout',
+                ]
 
-        # if self.repos:
-        #     if isinstance(self.repos, list):
-        #         for i in self.repos:
-        #             pass
+                question = {
+                    'type': 'list',
+                    'name': 'tmp',
+                    'message': "What to do?",
+                    'choices': git_commands,
+                }
+                self.command = prompt([question])['tmp']
+                if self.command == '<custom input>':
+                    question = {
+                        'type': 'input',
+                        'name': 'tmp',
+                        'message': "Enter git command: ",
+                    }
+                    self.command = prompt([question])['tmp']
+            else:
+                print(
+                    f"No command given in mete_hook for key = {self.key}.  Defaults to clone."
+                )
+                self.command = 'clone'
+
+        if self.repo_tree:
+            self.repo_tree = flatten_repo_tree(self.repo_tree)
+
+        for k, v in self.repo_tree.items():
+            path, folder_name = os.path.split(k)
+            if not os.path.isdir(path):
+                pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+
+            if isinstance(v, str):
+                v = self.update_repo_name(v)
+                with work_in(path):
+                    self.run_git_command(v, folder_name)
+
+            if isinstance(v, dict):
+                r = Repo(**v)
+                r.src = self.update_repo_name(r.src)
+                with work_in(path):
+                    self.run_git_command(r.src, folder_name, r.branch)
--- tackle/providers/git/tests/test_provider_github_hooks.py
+++ tackle/providers/git/tests/test_provider_github_hooks.py
@@ -3,8 +3,26 @@
 """Tests dict input objects for `tackle.providers.github.hooks` modules."""
 import os
 from tackle.main import tackle
+import pytest
+from tackle.utils.paths import rmtree
 
 
-def test_provider_git_hooks_repos(change_dir):
+@pytest.fixture()
+def clean_outputs():
+    yield
+    paths = ['services', 'foo', 'tackle-pypackage']
+    for p in paths:
+        if os.path.isdir(p):
+            rmtree(p)
+
+
+def test_provider_git_hooks_repos(change_dir, clean_outputs):
     """Verify hook."""
     output = tackle('.', context_file='repo.yaml')
+    assert os.path.isdir('tackle-pypackage')
+
+
+def test_provider_git_hooks_meta(change_dir, clean_outputs):
+    """Verify hook."""
+    output = tackle('.', context_file='meta.yaml', no_input=True)
+    assert os.path.isdir(os.path.join('foo', 'bar', 'thing'))
