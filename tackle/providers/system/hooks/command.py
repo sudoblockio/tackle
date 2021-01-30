3fdef57abb38e360bfceabb4aa94685ce37ecb7f
rob <robcannonxyz@gmail.com>
2021-01-30 14:58:29 -0700

    adding fixes to command hook for exiting on error

--- tackle/providers/system/hooks/command.py
+++ tackle/providers/system/hooks/command.py
@@ -18,6 +18,7 @@ from itertools import chain
 from select import select
 
 from tackle.models import BaseHook
+from tackle.exceptions import HookCallException
 
 if os.name != 'nt':
     # Don't import on windows as pty is not available there
@@ -43,13 +44,19 @@ class CommandHook(BaseHook):
     type: str = 'command'
 
     command: str
+    ignore_error: bool = False
 
     def execute(self):
-        p = subprocess.Popen(self.command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p = subprocess.Popen(self.command,
+                             shell=True,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
         output, err = p.communicate()
 
-        if err:
-            sys.exit(err)
+        if err and not self.ignore_error:
+            raise HookCallException(err.decode('utf-8'))
+        if err and self.ignore_error:
+            return err.decode('utf-8')
 
         return output.decode("utf-8")
 
@@ -67,6 +74,7 @@ class ShellHook(BaseHook):
     type: str = 'shell'
 
     command: str
+    ignore_error: bool = False
 
     def _set_size(self, fd):
         """Found at: https://stackoverflow.com/a/6420070."""
@@ -85,35 +93,46 @@ class ShellHook(BaseHook):
         # cmds = self.command.split('\n')
         # for cmd in cmds:
         # TODO: Fix multi-line calls
-        cmd = self.command.split()
 
-        with subprocess.Popen(
-            cmd, stdin=slaves[0], stdout=slaves[0], stderr=slaves[1]
-        ) as p:
-            for fd in slaves:
-                os.close(fd)  # no input
-                readable = {
-                    masters[0]: sys.stdout.buffer,  # store buffers seperately
-                    masters[1]: sys.stderr.buffer,
-                }
-            while readable:
-                for fd in select(readable, [], [])[0]:
-                    try:
-                        data = os.read(fd, 1024)  # read available
-                    except OSError as e:
-                        if e.errno != errno.EIO:
-                            raise  # XXX cleanup
-                        del readable[fd]  # EIO means EOF on some systems
-                    else:
-                        if not data:  # EOF
-                            del readable[fd]
+        cmd = self.command.replace('\n', ' ; ').split()
+
+        try:
+            with subprocess.Popen(
+                cmd, stdin=slaves[0], stdout=slaves[0], stderr=slaves[1]
+            ) as p:
+                for fd in slaves:
+                    os.close(fd)  # no input
+                    readable = {
+                        masters[0]: sys.stdout.buffer,  # store buffers seperately
+                        masters[1]: sys.stderr.buffer,
+                    }
+                while readable:
+                    for fd in select(readable, [], [])[0]:
+                        try:
+                            data = os.read(fd, 1024)  # read available
+                        except OSError as e:
+                            if e.errno != errno.EIO:
+                                raise  # XXX cleanup
+                            del readable[fd]  # EIO means EOF on some systems
                         else:
-                            if fd == masters[0]:  # We caught stdout
-                                click.echo(data.rstrip())
-                            else:  # We caught stderr
-                                click.echo(data.rstrip(), err=True)
-                            readable[fd].flush()
-        for fd in masters:
-            os.close(fd)
-        logger.debug("Process exited with %s return code." % p.returncode)
-        return data.decode("utf-8")
+                            if not data:  # EOF
+                                del readable[fd]
+                            else:
+                                if fd == masters[0]:  # We caught stdout
+                                    click.echo(data.rstrip())
+                                else:  # We caught stderr
+                                    click.echo(data.rstrip(), err=True)
+                                readable[fd].flush()
+            for fd in masters:
+                os.close(fd)
+            logger.debug("Process exited with %s return code." % p.returncode)
+            if p.returncode != 0 and not self.ignore_error:
+                raise HookCallException(data.decode('utf-8'))
+            else:
+                return data.decode("utf-8")
+
+        except Exception as e:
+            if self.ignore_error:
+                return e
+            else:
+                raise HookCallException(e)
--- tackle/providers/system/tests/command/test_provider_system_command.py
+++ tackle/providers/system/tests/command/test_provider_system_command.py
@@ -3,6 +3,7 @@
 """Tests dict input objects for `tackle.providers.system.hooks.command` module."""
 import os
 import pytest
+from tackle.exceptions import HookCallException
 
 from tackle.main import tackle
 
@@ -12,7 +13,7 @@ if os.name == 'nt':
 
 def test_provider_system_hook_command(change_dir):
     """Verify the hook call works properly."""
-    context = tackle('.', no_input=True)
+    context = tackle(no_input=True)
 
     assert 'tackle.yaml' in context['shell']
     assert 'tackle.yaml' in context['cmd']
@@ -20,5 +21,35 @@ def test_provider_system_hook_command(change_dir):
 
 def test_provider_system_hook_command_multi_line(change_dir):
     """Verify the hook call works properly."""
-    context = tackle('.', context_file='multi-line-cmd.yaml', no_input=True)
+    context = tackle(context_file='multi-line-cmd.yaml', no_input=True)
     assert 'No such file' not in context['shell']
+
+
+def test_provider_system_hook_command_exit(change_dir):
+    """Verify the hook call works properly."""
+    with pytest.raises(HookCallException):
+        tackle(context_file='command-exit.yaml', no_input=True)
+
+
+def test_provider_system_hook_shell_exit(change_dir):
+    """Verify the hook call works properly."""
+    with pytest.raises(HookCallException):
+        tackle(context_file='shell-exit.yaml', no_input=True)
+
+
+def test_provider_system_hook_shell_exit_long(change_dir):
+    """Verify the hook call works properly."""
+    with pytest.raises(HookCallException):
+        tackle(context_file='shell-exit-long.yaml', no_input=True)
+
+
+def test_provider_system_hook_shell_exit_ignore(change_dir):
+    """Verify the hook call works properly."""
+    o = tackle(context_file='shell-exit-ignore.yaml', no_input=True)
+    assert o
+
+
+def test_provider_system_hook_command_exit_ignore(change_dir):
+    """Verify the hook call works properly."""
+    o = tackle(context_file='command-exit-ignore.yaml', no_input=True)
+    assert o
