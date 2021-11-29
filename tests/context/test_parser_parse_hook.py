e69b7b8bc17db18c3111e45524c5eb52d041269c
robcxyz <rob@geometrylabs.io>
2021-11-29 00:53:13 -0800

    chore: update high level tests

--- tests/context/test_parser_parse_hook.py
+++ tests/context/test_parser_parse_hook.py
@@ -1,27 +1,25 @@
+"""High level tests for parser logic."""
 import pytest
 import yaml
 
-from tackle.models import Context
 from tackle.main import tackle
 
 FIXTURES = [
     ('tackle_map.yaml', 'tackle_map_output.yaml'),
     ('tackle_map_lists.yaml', 'tackle_map_lists_output.yaml'),
-    # ('tackle_map_root.yaml', 'tackle_map_root_output.yaml'),
-    # ('tackle_import.yaml', 'petstore.yaml'),
+    ('tackle_map_root.yaml', 'tackle_map_root_output.yaml'),
+    ('outer_tackle.yaml', 'outer_tackle_expected.yaml'),
+    # Non tackle things
+    ('k8s-deployment.yaml', 'k8s-deployment.yaml'),
+    ('docker-compose.yml', 'docker-compose.yml'),
 ]
 
 
 @pytest.mark.parametrize("fixture,expected_output", FIXTURES)
 def test_main_expected_output(change_curdir_fixtures, fixture, expected_output):
+    """Input equals output."""
     with open(expected_output) as f:
         expected_output = yaml.safe_load(f)
 
     output = tackle(fixture)
     assert dict(output) == expected_output
-
-
-def test_main_loops(change_curdir_fixtures):
-    # with open('tackle_map_lists.yaml') as f:
-    #     output = yaml.safe_load(f)
-    output = tackle('tackle_map_lists.yaml')
