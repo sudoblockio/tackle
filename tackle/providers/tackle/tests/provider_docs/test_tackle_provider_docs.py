1c41f62e98eebb46031123f473569bb5e4754c40
robcxyz <rob@geometrylabs.io>
2022-01-16 17:44:06 -0800

    chore: update provider docs

--- tackle/providers/tackle/tests/provider_docs/test_tackle_provider_docs.py
+++ tackle/providers/tackle/tests/provider_docs/test_tackle_provider_docs.py
@@ -1,9 +1,14 @@
 """test for provider_docs hook."""
+import sys
+
 from tackle import tackle
 
 
 def test_provider_tackle_provider_docs(change_dir):
     """Run the provider docs."""
+    if sys.version_info.minor <= 6:
+        return
+
     output = tackle('docs.yaml')
     assert (
         len(
