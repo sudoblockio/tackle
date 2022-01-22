74c9ac755077caab3a5c1839c0f023406be22426
robcxyz <rob@geometrylabs.io>
2022-01-22 15:29:32 -0800

    refactor: web-browser to webbrowser per native api

--- tackle/providers/system/hooks/web.py
+++ tackle/providers/system/hooks/web.py
@@ -1,18 +1,16 @@
 """Web hooks."""
-import logging
 import webbrowser
-from pydantic import AnyUrl, Field
 
-from tackle.models import BaseHook
-
-logger = logging.getLogger(__name__)
+from tackle.models import BaseHook, Field
 
 
 class WebBrowserHook(BaseHook):
     """Hook for registering a variable based on an input. Useful with rendering."""
 
     hook_type: str = 'webbrowser'
-    url: AnyUrl = Field(..., description="String url to open in browser.")
+    url: str = Field(..., description="String url to open in browser.")
+
+    _args = ['url']
 
     def execute(self):
         webbrowser.open(self.url, new=2)
