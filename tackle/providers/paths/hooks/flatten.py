c3c82c4024c63f6004c1a7ab1ab92bad4076428d
robcxyz <rob@geometrylabs.io>
2022-02-27 20:52:47 -0800

    chore: add list inputs to flatten_paths hook

--- tackle/providers/paths/hooks/flatten.py
+++ tackle/providers/paths/hooks/flatten.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 
 from tackle.models import BaseHook, Field
 
@@ -7,9 +8,9 @@ class FlattenPathHook(BaseHook):
     """Hook for flattening a dictionary into a list of paths."""
 
     hook_type: str = 'flatten_paths'
-    paths: dict = Field(
+    paths: Union[dict, list] = Field(
         ...,
-        description="A dictionary representing paths to files that should be flattened",
+        description="A list or map representing paths to files that should be flattened",
     )
     base_path: str = Field('', description="A base path to prefix in the output.")
 
@@ -17,15 +18,23 @@ class FlattenPathHook(BaseHook):
 
     def flatten_repo_tree(self, output: list, d, parent_key=''):
         """Flatten a dict to so that keys become nodes in a path."""
-        for k, v in d.items():
-            new_key = os.path.join(parent_key, k)
-            if isinstance(v, str):
-                output.append(os.path.join(new_key, v))
-            else:
-                self.flatten_repo_tree(output, v, new_key)
+        if isinstance(d, dict):
+            for k, v in d.items():
+                new_key = os.path.join(parent_key, k)
+                if isinstance(v, str):
+                    output.append(os.path.join(new_key, v))
+                else:
+                    self.flatten_repo_tree(output, v, new_key)
+
+        elif isinstance(d, list):
+            for i in d:
+                if isinstance(i, str):
+                    output.append(os.path.join(parent_key, i))
+                else:
+                    self.flatten_repo_tree(output, i, parent_key)
+
         return output
 
     def execute(self) -> list:
         output = []
-
         return self.flatten_repo_tree(output, self.paths, self.base_path)
--- tackle/providers/paths/tests/flatten/test_provider_paths_flatten_paths.py
+++ tackle/providers/paths/tests/flatten/test_provider_paths_flatten_paths.py
@@ -4,4 +4,6 @@ from tackle.main import tackle
 def test_provider_paths_flatten(change_dir):
     output = tackle()
     assert output['flattened'][0] == 'one/baz/stuff.yaml'
+    assert output['flattened'][9] == 'complex/alist/another_list/things.py'
     assert output['flattened_base'][0] == 'base/path/one/baz/stuff.yaml'
+    assert output['flattened_base_list'][0] == 'base/path/foo/bar.py'
