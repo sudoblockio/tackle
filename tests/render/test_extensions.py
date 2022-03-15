22ab38ce03430f1275b4c584aba8080b41115130
robcxyz <rob@geometrylabs.io>
2022-03-14 18:14:25 -0700

    chore: clean tests for extensions

--- tests/render/test_extensions.py
+++ tests/render/test_extensions.py
@@ -3,38 +3,14 @@ import pytest
 from tackle import tackle
 
 
-def test_render_extensions_base(change_curdir_fixtures):
-    """Render non-hook extensions. Legacy from CC."""
-    output = tackle('extensions.yaml')
-    assert output['jsonify'].startswith('json\n"{')
-
-
 def test_render_globals_base(change_curdir_fixtures):
-    """Test rendering globals."""
-    output = tackle('globals.yaml')
-    for i in output['globals_raw']:
-        assert i == 'stuff'
-
-
-# https://github.com/robcxyz/tackle-box/issues/19
-def test_render_globals_base_with_overlapping_context(change_curdir_fixtures):
     """
     Verify that when we have a variable in the context, for instance `namespace`, that
      we are able to render it from the context.
-
-     This requires either:
-        1. Checking the context if any of these vars exist
-            - Ambiguous, could have multiple contexts to check
-            - Could be
-        2. Checking the renderable string for global refs
-            - There are only 6 of them - could be compiled in a regex easily
-            - If the global var is not called (ie namespace()), then string replace that
-            - Build exception context to render the string replacement from the context
-
-    Option 2 seems to be the cleanest.
     """
     output = tackle('globals.yaml')
-    assert output
+    for i in output['globals_raw']:
+        assert i == 'stuff'
 
 
 def test_render_hooks_no_args(change_curdir_fixtures):
@@ -48,9 +24,19 @@ def test_render_hooks_with_args(change_curdir_fixtures):
     output = tackle('hooks-args.yaml')
     assert output['get'] == 'things'
     assert output['get_sep'] == 'things'
+    assert 'tmp' in output['nested']
+    assert output['nested_test']['and'] == 'things'
 
 
 def test_render_hooks_with_args_too_many(change_curdir_fixtures):
     """Verify exception raised with too many args."""
     with pytest.raises(Exception):
         tackle('hooks-args-too-many-args.yaml')
+
+
+def test_render_hooks_missing_args(change_curdir_fixtures):
+    """Verify exception raised when hook is missing / has wrong args."""
+    from pydantic import ValidationError
+
+    with pytest.raises(ValidationError):
+        tackle('hooks-missing-args.yaml')
