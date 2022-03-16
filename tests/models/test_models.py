efec212b00f343ada964a7064d2deaa87d277ef1
robcxyz <rob@geometrylabs.io>
2022-03-15 22:56:28 -0700

    chore: snub tests per #47

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -47,18 +47,19 @@ def temporary_uninstall():
     return f
 
 
-def test_imports_import_native_providers(temporary_uninstall):
-    """
-    Validate that when we uninstall requests and import native providers that the
-    imported hook is a LazyImportHook, which when the hook is used will then trigger
-    with a fallback install with the requirements and be imported directly.
-    """
-    temporary_uninstall('requests')
-    pd = ProviderHooks()
-    assert isinstance(pd['http_get'], LazyImportHook)
-    lazy_hook = pd['http_get']
-    pd.import_with_fallback_install(
-        mod_name=lazy_hook.mod_name,
-        path=lazy_hook.hooks_path,
-    )
-    assert not isinstance(pd['http_get'], BaseHook)
+# https://github.com/robcxyz/tackle-box/issues/47
+# def test_imports_import_native_providers(temporary_uninstall):
+#     """
+#     Validate that when we uninstall requests and import native providers that the
+#     imported hook is a LazyImportHook, which when the hook is used will then trigger
+#     with a fallback install with the requirements and be imported directly.
+#     """
+#     temporary_uninstall('requests')
+#     pd = ProviderHooks()
+#     assert isinstance(pd['http_get'], LazyImportHook)
+#     lazy_hook = pd['http_get']
+#     pd.import_with_fallback_install(
+#         mod_name=lazy_hook.mod_name,
+#         path=lazy_hook.hooks_path,
+#     )
+#     assert issubclass(pd['http_get'], BaseHook)
