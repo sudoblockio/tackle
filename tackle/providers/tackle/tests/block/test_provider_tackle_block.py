651f72e477944339d82974ece8933a5632c0ac36
robcxyz <rob@geometrylabs.io>
2022-03-20 15:39:38 -0700

    chore: add new block write tests

--- tackle/providers/tackle/tests/block/test_provider_tackle_block.py
+++ tackle/providers/tackle/tests/block/test_provider_tackle_block.py
@@ -3,17 +3,19 @@ from tackle.main import tackle
 
 def test_provider_system_hook_block_tackle(change_dir):
     """Simple block test."""
-    output = tackle('basic.yaml', no_input=True)
+    output = tackle('merge.yaml', no_input=True)
 
     assert output['stuff'] == 'here'
-    assert 'things' not in output
+
+    # assert 'things' not in output
 
 
 def test_provider_system_hook_block_embedded_blocks(change_dir):
     """Embedded with multiple blocks."""
     output = tackle('embedded_blocks.yaml', no_input=True)
 
-    assert output['things'] == 'things'
+    assert output['stuff'] == 'things'
+    assert output['blocker']['things'] == 'things'
 
 
 def test_provider_system_hook_block_looped(change_dir):
@@ -21,6 +23,8 @@ def test_provider_system_hook_block_looped(change_dir):
     output = tackle('looped.yaml', no_input=True)
 
     assert len(output['blocker']) == 2
+    assert output['blocker'][0]['first'] == 'stuff'
+    assert output['blocker'][1]['first'] == 'things'
 
 
 def test_provider_system_hook_block_block_merge(change_dir):
