8629541c89d9ab9f5b93af35041111f4e8e281f1
robcxyz <rob@geometrylabs.io>
2022-03-26 16:55:22 -0700

    chore: clean function tests with assertions

--- tests/functions/test_functions.py
+++ tests/functions/test_functions.py
@@ -4,35 +4,16 @@ from ruamel.yaml import YAML
 from tackle import tackle
 
 FIXTURES = [
-    # 'extensions.yaml',
-    # 'model-function.yaml',
-    'function-inline-call.yaml',
+    ('call.yaml', 'call-output.yaml'),
+    ('return.yaml', 'return-output.yaml'),
 ]
 
 
-@pytest.mark.parametrize("fixture", FIXTURES)
-def test_function_model_extraction(change_curdir_fixtures, fixture):
-    output = tackle(fixture)
-    assert output
-
-
-FIXTURES_ARGS = [
-    ('model-function.yaml', ['some_function']),
-]
-
-
-@pytest.mark.parametrize("fixture,args", FIXTURES_ARGS)
-def test_function_arguments(change_curdir_fixtures, fixture, args):
-
-    output = tackle(fixture, *args)
-    assert output
-
-
-def test_create_function_model(change_curdir_fixtures):
+@pytest.mark.parametrize("fixture,expected_output", FIXTURES)
+def test_function_model_extraction(change_curdir_fixtures, fixture, expected_output):
     yaml = YAML()
-    with open('model-function.yaml') as f:
-        model_fixture = yaml.load(f)
-    from tackle.parser import create_function_model
+    with open(expected_output) as f:
+        expected_output_dict = yaml.load(f)
 
-    output = create_function_model('foo', model_fixture['some_function<-'])
-    print()
+    output = tackle(fixture)
+    assert output == expected_output_dict
