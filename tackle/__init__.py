cba767f6db7efb30303bc9e74fd30f8010de53b1
robcxyz <rob@geometrylabs.io>
2022-04-17 23:37:58 -0700

    chore: version bump 0.3.0-alpha.1

--- tackle/__init__.py
+++ tackle/__init__.py
@@ -1,8 +1,9 @@
 """Main package for tackle box."""
-__version__ = "0.3.0-alpha.0"
+__version__ = "0.3.0-alpha.1"
 
 from tackle.models import BaseHook
 from tackle.models import Field
+from tackle.exceptions import HookCallException
 
 from tackle.main import tackle
 
