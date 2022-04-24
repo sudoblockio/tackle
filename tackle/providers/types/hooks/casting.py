0d7a884aa587359cf63eaa98bca0c310f7cf36a3
robcxyz <rob@geometrylabs.io>
2022-04-23 23:15:26 -0700

    chore: add casting hooks in type provider

---
+++ tackle/providers/types/hooks/casting.py
@@ -0,0 +1,37 @@
+"""Data type hook."""
+from typing import Any
+from pydantic import Field
+from tackle.models import BaseHook
+
+
+class IntegerHook(BaseHook):
+    """Hook for casting a variable to an integer type."""
+
+    hook_type: str = 'int'
+    input: Any = Field(..., description="Any variable input.", render_by_default=True)
+    args: list = ['input']
+
+    def exec(self) -> int:
+        return int(self.input)
+
+
+class FloatHook(BaseHook):
+    """Hook for casting a variable to a float type."""
+
+    hook_type: str = 'float'
+    input: Any = Field(..., description="Any variable input.", render_by_default=True)
+    args: list = ['input']
+
+    def exec(self) -> float:
+        return float(self.input)
+
+
+class BoolHook(BaseHook):
+    """Hook for casting a variable to a bool type."""
+
+    hook_type: str = 'bool'
+    input: Any = Field(..., description="Any variable input.", render_by_default=True)
+    args: list = ['input']
+
+    def exec(self) -> bool:
+        return bool(self.input)
--- tackle/providers/types/tests/test_provider_types.py
+++ tackle/providers/types/tests/test_provider_types.py
@@ -1,9 +1,8 @@
-"""Tests `import` in the `tackle.providers.tackle.hooks.match` hook."""
 from tackle import tackle
 
 
 def test_provider_logic_type(change_dir):
-    """Check assertions."""
+    """Check type works."""
     output = tackle('type.yaml')
     assert output['str_type'] == 'str'
     assert output['list_type'] == 'list'
@@ -12,3 +11,10 @@ def test_provider_logic_type(change_dir):
     assert output['list_dict_type'] == 'list'
     assert output['with_args']['type'] == 'list'
     assert output['default']['type'] == 'list'
+
+
+def test_hook_casting(change_dir):
+    """Check casting works."""
+    output = tackle('castings.yaml')
+    # Assertions in file
+    assert output
