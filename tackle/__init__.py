bcc7046f4c4fb3c3c1895ed02a093ecec3cac256
robcxyz <rob@sudoblock.io>
2022-08-18 22:37:10 -0600

    ci: update changelog action and version bump 0.3.1

--- tackle/__init__.py
+++ tackle/__init__.py
@@ -1,5 +1,5 @@
 """Main package for tackle box."""
-__version__ = "0.3.0-beta.6"
+__version__ = "0.3.1"  # x-release-please-version
 
 from tackle.models import BaseHook
 from tackle.models import Field
