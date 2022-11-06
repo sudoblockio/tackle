9b718aaf51cdb9922524d53ade292ff0eb8e4f55
robcxyz <rob@sudoblock.io>
2022-11-06 17:14:16 +0530

    chore: fix macro test

--- tests/macros/test_macros.py
+++ tests/macros/test_macros.py
@@ -1,16 +1,17 @@
 from ruamel.yaml import YAML
 
 from tackle.macros import function_field_to_parseable_macro
+from tackle.models import Context
 
 
-def test_function_field_to_parseable_macro(change_curdir_fixtures, context):
+def test_function_field_to_parseable_macro(change_curdir_fixtures):
     """Test that the macro is able to expand all the keys properly."""
     yaml = YAML()
     with open('func-inputs-basic.yaml') as f:
         fixture = yaml.load(f)
 
     output = function_field_to_parseable_macro(
-        fixture['input'], context=context, func_name='f'
+        fixture['input'], context=Context(), func_name='f'
     )
 
     assert fixture['expected'] == output
