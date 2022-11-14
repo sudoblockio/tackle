9c840423a47fd2bac7d2dfb7e50441b96ef83832
robcxyz <rob@sudoblock.io>
2022-11-14 17:24:00 +0530

    chore: add test for update_section hook

---
+++ tackle/providers/generate/tests/update_section/multi-line.md
@@ -0,0 +1,14 @@
+
+foo bar
+
+[//]: # (--start--)
+
+| Name | Description |
+|---|---|
+| Stuff | A thing |
+| Thing | A stuff |
+
+[//]: # (--end--)
+
+foo baz
+
--- tackle/providers/generate/tests/update_section/test_provider_generate_update_section.py
+++ tackle/providers/generate/tests/update_section/test_provider_generate_update_section.py
@@ -11,3 +11,14 @@ def test_hook_generate_update_document(change_dir):
 
     assert output
     assert file == expected_output
+
+
+def test_hook_generate_update_document_multi_line(change_dir):
+    """Check multi-line capability in arg."""
+    output = tackle("multi-line.yaml", "update_readme")
+
+    with open('multi-line.md') as f:
+        file = f.read()
+
+    assert '| Stuff | A thing |' in file
+    assert output
