bb53f3d90e0a672b96b02f6ea1e047814bd83f02
robcxyz <rob@sudoblock.io>
2022-12-28 17:47:43 +0530

    provider: add more options to filter hidden files / dirs in  hook

--- tackle/providers/paths/hooks/listdir.py
+++ tackle/providers/paths/hooks/listdir.py
@@ -9,18 +9,43 @@ class ListdirHook(BaseHook):
     hook_type: str = 'listdir'
     path: str = Field('.', description="Path to directory to list contents.")
     sort: bool = Field(False, description="Boolean to sort the output")
-    ignore_hidden_files: bool = Field(
-        None, description="Boolean to ignore hidden files"
+    ignore_hidden: bool = Field(
+        False, description="Boolean to ignore hidden files or directories"
     )
+    ignore_hidden_files: bool = Field(
+        False, description="Boolean to ignore hidden files"
+    )
+    ignore_hidden_directories: bool = Field(
+        False, description="Boolean to ignore hidden directories"
+    )
+    only_files: bool = Field(False, description="Only return files.")
+    only_directories: bool = Field(False, description="Only return directories.")
 
     args: list = ['path']
     _docs_order = 2
 
     def exec(self) -> list:
-        files = os.listdir(os.path.expanduser(self.path))
+        files = os.listdir(os.path.expanduser(os.path.expandvars(self.path)))
         if self.sort:
             files.sort()
+        if self.ignore_hidden:
+            files = [f for f in files if not f.startswith('.')]
         if self.ignore_hidden_files:
-            return [f for f in files if not f.startswith('.')]
-        else:
-            return files
+            files = [
+                f
+                for f in files
+                if not (
+                    f.startswith('.') and os.path.isfile(os.path.join(self.path, f))
+                )
+            ]
+        if self.ignore_hidden_directories:
+            files = [
+                f
+                for f in files
+                if not (f.startswith('.') and os.path.isdir(os.path.join(self.path, f)))
+            ]
+        if self.only_files:
+            files = [f for f in files if os.path.isfile(os.path.join(self.path, f))]
+        if self.only_directories:
+            files = [f for f in files if os.path.isdir(os.path.join(self.path, f))]
+        return files
