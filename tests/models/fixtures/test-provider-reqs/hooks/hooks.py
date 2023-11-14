f6a63000cbabb7582533cfc20715e4fc23c09adb
robcxyz <rob@sudoblock.io>
2023-11-14 14:03:39 +0800

    chore: remove all fixtures and tests from tests/models

--- tests/models/fixtures/test-provider-reqs/hooks/hooks.py
+++
@@ -1,9 +0,0 @@
-from tackle import BaseHook
-from art import art
-
-
-class ArtHook(BaseHook):
-    hook_name: str = 'art'
-
-    def exec(self):
-        return art("random")
--- tests/models/test_models_2.py
+++
@@ -1,9 +0,0 @@
-from tackle.models import DclHookInput
-
-
-def test_dcl_hook_input_extra():
-    h = DclHookInput(
-        **{'foo': 1, 'extends': 'bar'}
-    )
-
-    assert h
