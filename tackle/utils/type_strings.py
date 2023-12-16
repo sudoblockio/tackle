b29c4b97c46de058494cfbb52d780d78866749f6
robcxyz <rob@sudoblock.io>
2023-12-16 23:56:37 +0700

    chore: split out type to string logic into own util to be used in help and docs

---
+++ tackle/utils/type_strings.py
@@ -0,0 +1,38 @@
+from typing import Any, _GenericAlias, List, _UnionGenericAlias
+from types import UnionType
+
+from pydantic.fields import FieldInfo
+
+
+def type_to_string(type_: type) -> str:
+    if type_ == Any:
+        output = 'Any'
+    elif isinstance(type_, _UnionGenericAlias):
+        output = type_.__repr__()
+    elif isinstance(type_, _GenericAlias):
+        if isinstance(type_, List):
+            output = 'list'
+        else:
+            output = type_.__repr__()
+    elif isinstance(type_, UnionType):
+        if isinstance(type_, List):
+            output = 'list'
+        else:
+            output = type_.__repr__()
+    else:
+        output = type_.__name__
+    if 'typing.' in output:
+        output = output.replace('typing.', '')
+    return output
+
+
+def field_to_string(field: FieldInfo) -> str:
+    """
+    Convert hook ModelField type_ to string. This is used in a couple places such as
+     the help dialogue and local documentation hooks.
+    """
+    if isinstance(field.json_schema_extra, dict) and 'type' in field.json_schema_extra:
+        # We have a declarative hook type
+        return field.json_schema_extra['type']
+    type_ = field.annotation
+    return type_to_string(type_=type_)
---
+++ tests/utils/test_utils_type_strings.py
@@ -0,0 +1,64 @@
+import pytest
+from ipaddress import IPv4Address
+from typing import Dict, List, Union, Any
+from pydantic import BaseModel
+
+from tackle import BaseHook, tackle
+from tackle.hooks import create_dcl_hook
+from tackle.utils.type_strings import field_to_string, type_to_string
+
+TYPE_FIXTURES = [
+    (dict, "dict"),
+    (Dict, "Dict"),
+    (list, "list"),
+    (List, "List"),
+    (Any, "Any"),
+    (IPv4Address, "IPv4Address"),
+    (Union[dict, list], "Union[dict, list]"),
+    (str | int, "str | int"),
+]
+
+
+@pytest.mark.parametrize("type_,expected_output", TYPE_FIXTURES)
+def test_hook_type_to_string(type_, expected_output):
+    """Check we can convert a type object to string properly."""
+    output = type_to_string(type_)
+    assert output == expected_output
+
+
+def test_hook_type_to_string_python_types():
+    class Foo(BaseModel):
+        foo: str
+
+    class MyHook(BaseHook):
+        a_str: str
+        a_union: Union[str, int]
+        a_new_union: str | int
+        embedded: Foo
+        embedded_list: list[Foo]
+
+    my_hook_fields = MyHook.model_fields
+
+    assert field_to_string(my_hook_fields['a_str']) == 'str'
+    assert field_to_string(my_hook_fields['a_new_union']) == 'str | int'
+    assert field_to_string(my_hook_fields['a_union']) == 'Union[str, int]'
+    assert field_to_string(my_hook_fields['embedded']) == 'Foo'
+    # TODO: Broken - goes to list
+    # assert field_to_string(my_hook_fields['embedded_list']) == 'list[Foo]'
+
+
+def test_hook_type_to_string_dcl_types(cd_fixtures):
+    context = tackle('field-to-string-types.yaml', return_context=True)
+
+    Hook = context.hooks.public['MyHook']
+    Hook = create_dcl_hook(
+        context=context,
+        hook_name="MyHook",
+        hook_input_raw=Hook.input_raw,
+    )
+    my_hook_fields = Hook.model_fields
+
+    assert field_to_string(my_hook_fields['a_str']) == 'str'
+    assert field_to_string(my_hook_fields['a_new_union']) == 'str | int'
+    assert field_to_string(my_hook_fields['a_union']) == 'Union[str, int]'
+    assert field_to_string(my_hook_fields['embedded']) == 'Foo'
